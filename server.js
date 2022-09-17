f39f369949053ac970432a9cb9a483663b1189d2
root <root@5521694.lesfleursdelanuit.tech>
2022-09-17 05:04:18 -0500

    some edits

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import 'dotenv/config';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import express from 'express';
+
+
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
 
 app.get('/', (req, res) => {
     res
