254ba3a4e1fd5660bfece205c52429f2d7191cdf
root <root@5521694.lesfleursdelanuit.tech>
2022-09-17 03:53:40 -0500

    just a minor change for testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
     res
         .status(200)
-        .send('Hello server is running')
+        .send('Hello server is running on Heroku!!')
         .end();
 });
 
